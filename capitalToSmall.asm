e7e90ac85c92dad6d08919f54a1baf0751aaad0c
Asionsolver <asionsolver274@gmail.com>
2023-03-01 10:34:26 +0600

    Learn new topic like Capital to Small and Small to Capital letter

---
+++ capitalToSmall.asm
@@ -0,0 +1,37 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+
+
+MAIN PROC
+    MOV AH,1 ;INPUT
+    INT 21H
+
+    ADD AL, 20H ;CONVERSION
+    MOV AH,2 ;OUTPUT
+    MOV DL,AL
+    INT 21H  
+    
+     MOV AH,1 ;INPUT
+    INT 21H
+
+    ADD AL, 20H ;CONVERSION
+    MOV AH,2 ;OUTPUT
+    MOV DL,AL
+    INT 21H
+    
+     MOV AH,1 ;INPUT
+    INT 21H
+    ADD AL, 20H ;CONVERSION
+    
+    MOV AH,2 ;OUTPUT
+    MOV DL,AL
+    INT 21H
+    
+    MOV AH,4CH
+    INT 21H
+    
+  
+    MAIN ENDP
+          END MAIN
