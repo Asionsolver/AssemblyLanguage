cb95b098eb2af473cbd063f6c6babee1ad43f5b6
Asionsolver <asionsolver274@gmail.com>
2023-06-01 12:53:58 +0600

    add two file

--- Final/containChar.asm
+++ Final/containChar.asm
@@ -1,50 +1,41 @@
-.MODEL SMALL 
-.STACK 100H 
+;EX-6.9 
+;Write some code to count the number of characters in an input line
+
+.MODEL SMALL
+.STACK 100H
 .DATA
-
-    A    DB 10,13,'ENTER THE NUMBER: $'
-    C    DB 10,13,'RESULT $'
 .CODE
+
 MAIN PROC
-
-            MOV AX,@DATA
-            MOV DS,AX
-
-            MOV AH,9
-            LEA DX,A
-            INT 21H
+        
+        MOV CX, 30H
+        MOV AH, 1
+        INT 21H
+        
+    WHILE_:
     
-            MOV AH,1
-            INT 21H
+        CMP AL, 0DH
+        JE END_WHILE
+        
+        INC CX
+        INT 21H
+        JMP WHILE_
+        
+    END_WHILE:
+        
+        MOV AH, 2
+        MOV DL, 0DH
+        INT 21H
+        
+        MOV DL, 0AH
+        INT 21H
+        
+        MOV AH, 2
+        MOV DX, CX
+        INT 21H
+        
+        MOV AH, 4CH
+        INT 21H
     
-    
-            MOV AH,9
-            LEA DX,C
-            INT 21H
-    
-            CMP AL,1
-            JE  ODD
-            CMP AL,3
-            JE  ODD
-            CMP AL,2
-            JE  EVEN
-            CMP AL,4
-            JE  EVEN
-     
-    ODD:    
-            MOV DL,'o'
-            JMP DISPLAY
-     
-    EVEN:   
-            MOV DL,'e'
-            JMP DISPLAY
-    
-    DISPLAY:
-            MOV AH,2
-            INT 21h
-      
-    EXIT:   
-            MOV AH,4CH
-            INT 21H
 MAIN ENDP
-END MAIN
+    END MAIN
