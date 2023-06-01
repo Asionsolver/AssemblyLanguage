9d52af3ccee6e1be54a258ee2b6b66b1a5b3488d
Asionsolver <asionsolver274@gmail.com>
2023-06-01 12:16:42 +0600

    ADD TWO FILE ASM

--- Final/displayCharSequence.asm
+++ Final/displayCharSequence.asm
@@ -1,36 +1,26 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
-    CHAR_MSG DB 'The character that comes first is: $'
 
 .CODE
 
 MAIN PROC
-                 MOV AH, 0           ; Set AH to 0 for reading a character
-                 MOV DL, AL          ; Move the character from AL to DL
-                 INT 21H             ; Read a character from the input
+          MOV AH,1
+          INT 21H
+          CMP AL, 'y'
+          JE  THEN
 
-                 MOV AH, 0           ; Set AH to 0 for reading a character
-                 MOV DL, BL          ; Move the character from BL to DL
-                 INT 21H             ; Read a character from the input
+          CMP AL, 'Y'
+          JE  THEN
 
-                 CMP AL, BL          ; Compare the characters in AL and BL
-                 JBE DISPLAY_AL      ; Jump to DISPLAY_AL if AL <= BL
-                 JMP DISPLAY_BL      ; Otherwise, jump to DISPLAY_BL
+          JMP ELSE_
+    THEN: 
+          MOV DL, AL
+          MOV AH, 2
+          INT 21H
+    ELSE_:
+          MOV AH, 4CH
+          INT 21H
 
-    DISPLAY_AL:  
-                 MOV DL, AL          ; Move the character from AL to DL
-                 JMP DISPLAY_CHAR
-
-    DISPLAY_BL:  
-                 MOV DL, BL          ; Move the character from BL to DL
-
-    DISPLAY_CHAR:
-                 MOV AH, 2           ; Set AH to 2 for displaying a character
-                 INT 21H             ; Display the character in DL
-
-                 MOV AH, 4CH         ; Exit the program
-                 INT 21H
 MAIN ENDP
-
 END MAIN
